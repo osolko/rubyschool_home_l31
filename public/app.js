e1ece1b623a089ac51f303cf87cabb8230d225cb
osolko <o.savka@gmail.com>
2019-05-07 08:01:03 +0300

    play with local storage var

--- public/app.js
+++ public/app.js
@@ -10,3 +10,13 @@ function increment()
 		alert(x);
 		x= x+1;
 	}
+
+
+function something_1()
+	{
+		var x = window.localStorage.getItem('aaa');
+
+		window.localStorage.setItem('aaa',  555);
+		
+		alert(x);
+	}

138be5b277a8f7d21f1e26169f70ca6356e095c7
osolko <o.savka@gmail.com>
2019-05-14 10:31:14 +0300

    add rows to migration

--- db/migrate/20190514070221_add_products.rb
+++ db/migrate/20190514070221_add_products.rb
@@ -6,5 +6,7 @@ class AddProducts < ActiveRecord::Migration[5.2]
 :is_spicy => false, :is_veg   => false, :is_best_offer => false, :path_to_image => '/images/1.jpg'	
 
   	Product.create :title => 'Paperoni', :description => 'Its Paperoni pizza', :price => 8, :size  => 30, :is_spicy => true, :is_veg   => false, :is_best_offer => true, :path_to_image => '/images/2.jpg'	
+
+  	Product.create :title => 'Paperoni', :description => 'Its Paperoni pizza Big size', :price => 10, :size  => 60, :is_spicy => true, :is_veg   => false, :is_best_offer => false, :path_to_image => '/images/2.jpg'	
   end
 end

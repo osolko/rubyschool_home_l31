e39b237db9a55354e714ad9e08c6a4179b6ebff2
osolko <o.savka@gmail.com>
2019-05-21 08:13:10 +0300

    new func. cart_get_number_of_items

--- app.rb
+++ app.rb
@@ -24,6 +24,7 @@ get '/' do
 	erb :index
 end
 
+
 get '/about' do
 	erb :about
 end
--- public/app.js
+++ public/app.js
@@ -43,4 +43,23 @@ function add_to_cart(id)
 		x = x * 1 +1;
 		window.localStorage.setItem(key,  x);
 
+		alert('Item in cart: ' + cart_get_number_of_items())
+
+	}
+
+
+function cart_get_number_of_items()
+	{
+		var cnt = 0;
+		for(var i=0; i < window.localStorage.length; i++)	
+			{
+				var key = window.localStorage.key(i); 			//get key from hash
+				var value = window.localStorage.getItem(key); 	//get value from hash
+
+				if (key.indexOf('product_') == 0)
+					{
+						cnt = cnt + value * 1;
+					}
+			}
+			return cnt;
 	}

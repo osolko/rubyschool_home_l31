5faf0385f1791a852d81b1a01ac5d56530f39dd0
osolko <o.savka@gmail.com>
2019-05-21 08:40:04 +0300

    add func cart_get_orders

--- public/app.js
+++ public/app.js
@@ -63,3 +63,21 @@ function cart_get_number_of_items()
 			}
 			return cnt;
 	}
+
+
+function cart_get_orders()
+{
+	var orders = '';
+	for(var i=0; i < window.localStorage.length; i++)	
+		{
+			var key = window.localStorage.key(i); 			//get key from hash
+			var value = window.localStorage.getItem(key); 	//get value from hash
+
+			if (key.indexOf('product_') == 0)
+				{
+					orders = orders + key + value + ',' ;
+				}
+		}
+		return orders;
+}
+

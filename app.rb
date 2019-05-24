879224e8fc1d435a149152eae8f5cae3cd35735b
osolko <o.savka@gmail.com>
2019-05-24 09:07:18 +0300

    add parser for prod items

--- app.rb
+++ app.rb
@@ -11,9 +11,10 @@ class  Product < ActiveRecord::Base
 
 end
 
-# before do
-# 	@product = Product.all
-# end
+before do
+	@product = Product.all
+	#@item = 0
+end
 
 get '/' do
 
@@ -31,8 +32,38 @@ end
 
 
 post '/cart' do
-  erb "Hello World"
+  orders_input = params[:orders]
+  @items = parse_orders_input orders_input
+
+  @items.each do |item|
+    # id, cnt
+    item[0] = Product.find(item[0])
+  end
+
+	erb :cart
 end
+
+def parse_orders_input orders_input
+  s1 = orders_input.split(/,/)
+
+  arr = []
+
+  s1.each do |x|
+    s2 = x.split(/\=/)
+
+    s3 = s2[0].split(/_/)
+
+    id = s3[1]
+    cnt = s2[1]
+
+    arr2 = [id, cnt]
+
+    arr.push arr2
+  end
+
+  return arr
+end
+
 
 
 

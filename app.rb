28c650c9a8ea4cd4a545368ff34b5887cf00d6c3
osolko <o.savka@gmail.com>
2019-05-24 10:06:31 +0300

    add order form to cart

--- app.rb
+++ app.rb
@@ -30,8 +30,8 @@ end
 
 
 post '/cart' do
-  orders_input = params[:orders]
-  @items = parse_orders_input orders_input
+  @orders_input = params[:orders]
+  @items = parse_orders_input @orders_input
 
   @items.each do |item|
     # id, cnt
